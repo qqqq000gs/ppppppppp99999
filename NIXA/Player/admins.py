8f7586122eb4424dd6cbbaf762b6a3c53cd0b575
ERTWF <106486342+ERTWF@users.noreply.github.com>
2022-06-23 05:45:25 +0300

    Add files via upload

---
+++ NIXA/Player/admins.py
@@ -0,0 +1,297 @@
+from NIXA.Cache.admins import admins
+from NIXA.main import call_py
+from pyrogram import filters
+from NIXA.main import bot as Client
+from NIXA.decorators import authorized_users_only
+from NIXA.filters import command, other_filters
+from NIXA.queues import QUEUE, clear_queue
+from NIXA.utils import skip_current_song, skip_item
+from config import BOT_USERNAME, GROUP_SUPPORT, IMG_3, UPDATES_CHANNEL
+from pyrogram.types import (
+    CallbackQuery,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    Message,
+)
+
+
+bttn = InlineKeyboardMarkup(
+    [[InlineKeyboardButton("🔙 رجوع", callback_data="cbmenu")]]
+)
+
+
+bcl = InlineKeyboardMarkup(
+    [[InlineKeyboardButton("🗑 حذف", callback_data="cls")]]
+)
+
+
+@Client.on_message(command(["تحديث", f"adminchache"]) & other_filters)
+@authorized_users_only
+async def update_admin(client, message):
+    global admins
+    new_admins = []
+    new_ads = await client.get_chat_members(message.chat.id, filter="administrators")
+    for u in new_ads:
+        new_admins.append(u.user.id)
+    admins[message.chat.id] = new_admins
+    await message.reply_text(
+        "✅ بوت **تم تحديث !**\n✅ **البيانات**وتم تحديث**المشرفين!**"
+    )
+
+
+@Client.on_message(command(["تخطي", f"تخ", "التالي"]) & other_filters)
+@authorized_users_only
+async def skip(client, m: Message):
+
+    keyboard = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    text="تحكم", callback_data="cbmenu"
+                ),
+                InlineKeyboardButton(
+                    text="حذف", callback_data="cls"
+                ),
+            ]
+        ]
+    )
+
+    chat_id = m.chat.id
+    if len(m.command) < 2:
+        op = await skip_current_song(chat_id)
+        if op == 0:
+            await m.reply("❌ لايوجد شيئ شغال")
+        elif op == 1:
+            await m.reply("✅ __تم مسح القائمه__ **وتم تحكم.**\n\n**مغادرة المساعد من المحادثه الصوتيه**")
+        elif op == 2:
+            await m.reply("🗑️ **مسح قوائم الانتظار**\n\n**وغادر المساعد الدردشة الصوتيه**")
+        else:
+            await m.reply_photo(
+                photo=f"{IMG_3}",
+                caption=f"⏭ **تم التخطي للمسار التالي.**\n\n🏷 **الاسم:** [{op[0]}]({op[1]})\n💭 **الدردشة:** `{chat_id}`\n💡 **sᴛᴀᴛᴜs:** `ᴘʟᴀʏɪɴɢ`\n🎧 **طلب من المعلم:** {m.from_user.mention()}",
+                reply_markup=keyboard,
+            )
+    else:
+        skip = m.text.split(None, 1)[1]
+        OP = "🗑 **تم مسح الاغنيه في قائمة الانتظار:**"
+        if chat_id in QUEUE:
+            items = [int(x) for x in skip.split(" ") if x.isdigit()]
+            items.sort(reverse=True)
+            for x in items:
+                if x == 0:
+                    pass
+                else:
+                    hm = await skip_item(chat_id, x)
+                    if hm == 0:
+                        pass
+                    else:
+                        OP = OP + "\n" + f"**#{x}** - {hm}"
+            await m.reply(OP)
+
+
+@Client.on_message(
+    command(["ايقاف", f"اسكت", "انهاء", f"كافي", "وكف"])
+    & other_filters
+)
+@authorized_users_only
+async def stop(client, m: Message):
+    chat_id = m.chat.id
+    if chat_id in QUEUE:
+        try:
+            await call_py.leave_group_call(chat_id)
+            clear_queue(chat_id)
+            await m.reply("✅ طلب المعلم تم الايقاف.")
+        except Exception as e:
+            await m.reply(f"🚫 **ᴇʀʀᴏʀ:**\n\n`{e}`")
+    else:
+        await m.reply("❌ **ماني مشتغل**")
+
+
+@Client.on_message(
+    command(["مؤقت", f"pause@{BOT_USERNAME}", "vpause"]) & other_filters
+)
+@authorized_users_only
+async def pause(client, m: Message):
+    chat_id = m.chat.id
+    if chat_id in QUEUE:
+        try:
+            await call_py.pause_stream(chat_id)
+            await m.reply(
+                "⏸ **تم ايقاف المسار مؤقتا**\n\n• **, لأستئناف البث استخدم ملف **\n» /استمرار."
+            )
+        except Exception as e:
+            await m.reply(f"🚫 **ᴇʀʀᴏʀ:**\n\n`{e}`")
+    else:
+        await m.reply("❌ **ماكو يمعود**")
+
+
+@Client.on_message(
+    command(["استمرار", f"resume@{BOT_USERNAME}", "vresume"]) & other_filters
+)
+@authorized_users_only
+async def resume(client, m: Message):
+    chat_id = m.chat.id
+    if chat_id in QUEUE:
+        try:
+            await call_py.resume_stream(chat_id)
+            await m.reply(
+                "▶️ **تم الاستمرار.**\n\n• **لايقاف البث مؤقتا استخدم ملف**\n» /مؤقت."
+            )
+        except Exception as e:
+            await m.reply(f"🚫 **ᴇʀʀᴏʀ:**\n\n`{e}`")
+    else:
+        await m.reply("❌ **ماكو يمعود**")
+
+
+@Client.on_message(
+    command(["كتم", f"mute@{BOT_USERNAME}", "vmute"]) & other_filters
+)
+@authorized_users_only
+async def mute(client, m: Message):
+    chat_id = m.chat.id
+    if chat_id in QUEUE:
+        try:
+            await call_py.mute_stream(chat_id)
+            await m.reply(
+                "🔇 **طلب المعلم.**\n\n• **تم خروسي, لرفع كتمي اكتب**\n» /سولف."
+            )
+        except Exception as e:
+            await m.reply(f"🚫 **ᴇʀʀᴏʀ:**\n\n`{e}`")
+    else:
+        await m.reply("❌ **ماكو يمعود**")
+
+
+@Client.on_message(
+    command(["سولف", f"unmute@{BOT_USERNAME}", "vunmute"]) & other_filters
+)
+@authorized_users_only
+async def unmute(client, m: Message):
+    chat_id = m.chat.id
+    if chat_id in QUEUE:
+        try:
+            await call_py.unmute_stream(chat_id)
+            await m.reply(
+                "🔊 **طلب المعلم تم الاستمرار.**\n\n• **لكتمي مره اخره, اكتب**\n» /كتم."
+            )
+        except Exception as e:
+            await m.reply(f"🚫 **ᴇʀʀᴏʀ:**\n\n`{e}`")
+    else:
+        await m.reply("❌ **ماكو يمعود**")
+
+
+@Client.on_callback_query(filters.regex("cbpause"))
+async def cbpause(_, query: CallbackQuery):
+    if query.message.sender_chat:
+        return await query.answer("ʏᴏᴜ'ʀᴇ ᴀɴ ᴀɴᴏɴʏᴍᴏᴜs ᴀᴅᴍɪɴ !\n\n» ʀᴇᴠᴇʀᴛ ʙᴀᴄᴋ ᴛᴏ ᴜsᴇʀ ᴀᴄᴄᴏᴜɴᴛ ғʀᴏᴍ ᴀᴅᴍɪɴ ʀɪɢʜᴛs.")
+    a = await _.get_chat_member(query.message.chat.id, query.from_user.id)
+    if not a.can_manage_voice_chats:
+        return await query.answer("💡المسؤول الوحيد الذي لدي اذن ادارة الدردشات الصوتيه يمكنه النقر على هذا الزر !", show_alert=True)
+    chat_id = query.message.chat.id
+    if chat_id in QUEUE:
+        try:
+            await call_py.pause_stream(chat_id)
+            await query.edit_message_text(
+                "⏸ توقف البث مؤقتآ", reply_markup=bttn
+            )
+        except Exception as e:
+            await query.edit_message_text(f"🚫 **ᴇʀʀᴏʀ:**\n\n`{e}`", reply_markup=bcl)
+    else:
+        await query.answer("❌ لايوجد شيئ شغال", show_alert=True)
+
+
+@Client.on_callback_query(filters.regex("cbresume"))
+async def cbresume(_, query: CallbackQuery):
+    if query.message.sender_chat:
+        return await query.answer("ʏᴏᴜ'ʀᴇ ᴀɴ ᴀɴᴏɴʏᴍᴏᴜs ᴀᴅᴍɪɴ !\n\n» ʀᴇᴠᴇʀᴛ ʙᴀᴄᴋ ᴛᴏ ᴜsᴇʀ ᴀᴄᴄᴏᴜɴᴛ ғʀᴏᴍ ᴀᴅᴍɪɴ ʀɪɢʜᴛs.")
+    a = await _.get_chat_member(query.message.chat.id, query.from_user.id)
+    if not a.can_manage_voice_chats:
+        return await query.answer("💡المسؤول الوحيد الذي لدي اذن ادارة الدردشات الصوتيه يمكنه النقر على هذا الزر !", show_alert=True)
+    chat_id = query.message.chat.id
+    if chat_id in QUEUE:
+        try:
+            await call_py.resume_stream(chat_id)
+            await query.edit_message_text(
+                "▶️ تم استئناف البث", reply_markup=bttn
+            )
+        except Exception as e:
+            await query.edit_message_text(f"🚫 **ᴇʀʀᴏʀ:**\n\n`{e}`", reply_markup=bcl)
+    else:
+        await query.answer("❌ لايوجد شيئ شغال", show_alert=True)
+
+
+@Client.on_callback_query(filters.regex("cbstop"))
+async def cbstop(_, query: CallbackQuery):
+    if query.message.sender_chat:
+        return await query.answer("ʏᴏᴜ'ʀᴇ ᴀɴ ᴀɴᴏɴʏᴍᴏᴜs ᴀᴅᴍɪɴ !\n\n» ʀᴇᴠᴇʀᴛ ʙᴀᴄᴋ ᴛᴏ ᴜsᴇʀ ᴀᴄᴄᴏᴜɴᴛ ғʀᴏᴍ ᴀᴅᴍɪɴ ʀɪɢʜᴛs.")
+    a = await _.get_chat_member(query.message.chat.id, query.from_user.id)
+    if not a.can_manage_voice_chats:
+        return await query.answer("💡المسؤول الوحيد الذي لدي اذن ادارة الدردشات الصوتيه يمكنه النقر على هذا الزر !", show_alert=True)
+    chat_id = query.message.chat.id
+    if chat_id in QUEUE:
+        try:
+            await call_py.leave_group_call(chat_id)
+            clear_queue(chat_id)
+            await query.edit_message_text("✅ **طلب المعلم تم الايقاف**", reply_markup=bcl)
+        except Exception as e:
+            await query.edit_message_text(f"🚫 **ᴇʀʀᴏʀ:**\n\n`{e}`", reply_markup=bcl)
+    else:
+        await query.answer("❌ لايوجد شيئ شغال", show_alert=True)
+
+
+@Client.on_callback_query(filters.regex("cbmute"))
+async def cbmute(_, query: CallbackQuery):
+    if query.message.sender_chat:
+        return await query.answer("ʏᴏᴜ'ʀᴇ ᴀɴ ᴀɴᴏɴʏᴍᴏᴜs ᴀᴅᴍɪɴ !\n\n» ʀᴇᴠᴇʀᴛ ʙᴀᴄᴋ ᴛᴏ ᴜsᴇʀ ᴀᴄᴄᴏᴜɴᴛ ғʀᴏᴍ ᴀᴅᴍɪɴ ʀɪɢʜᴛs.")
+    a = await _.get_chat_member(query.message.chat.id, query.from_user.id)
+    if not a.can_manage_voice_chats:
+        return await query.answer("💡المسؤول الوحيد الذي لدي اذن ادارة الدردشات الصوتيه يمكنه النقر على هذا الزر !", show_alert=True)
+    chat_id = query.message.chat.id
+    if chat_id in QUEUE:
+        try:
+            await call_py.mute_stream(chat_id)
+            await query.edit_message_text(
+                "🔇 تم كتب صوت ᴜsᴇʀʙᴏᴛ بنجاح", reply_markup=bttn
+            )
+        except Exception as e:
+            await query.edit_message_text(f"🚫 **ᴇʀʀᴏʀ:**\n\n`{e}`", reply_markup=bcl)
+    else:
+        await query.answer("❌ لايوجد شيئ شغال", show_alert=True)
+
+
+@Client.on_callback_query(filters.regex("cbunmute"))
+async def cbunmute(_, query: CallbackQuery):
+    if query.message.sender_chat:
+        return await query.answer("ʏᴏᴜ'ʀᴇ ᴀɴ ᴀɴᴏɴʏᴍᴏᴜs ᴀᴅᴍɪɴ !\n\n» ʀᴇᴠᴇʀᴛ ʙᴀᴄᴋ ᴛᴏ ᴜsᴇʀ ᴀᴄᴄᴏᴜɴᴛ ғʀᴏᴍ ᴀᴅᴍɪɴ ʀɪɢʜᴛs.")
+    a = await _.get_chat_member(query.message.chat.id, query.from_user.id)
+    if not a.can_manage_voice_chats:
+        return await query.answer("💡 المسؤول الوحيد الذي لدي اذن ادارة الدردشات الصوتيه يمكنه النقر على هذا الزر !", show_alert=True)
+    chat_id = query.message.chat.id
+    if chat_id in QUEUE:
+        try:
+            await call_py.unmute_stream(chat_id)
+            await query.edit_message_text(
+                "🔊 تم الغاء كتم ᴜsᴇʀʙᴏᴛ بنجاح", reply_markup=bttn
+            )
+        except Exception as e:
+            await query.edit_message_text(f"🚫 **ᴇʀʀᴏʀ:**\n\n`{e}`", reply_markup=bcl)
+    else:
+        await query.answer("❌ لايوجد شيئ شغال", show_alert=True)
+
+
+@Client.on_message(
+    command(["ضبط", f"volume@{BOT_USERNAME}", "vol"]) & other_filters
+)
+@authorized_users_only
+async def change_volume(client, m: Message):
+    range = m.command[1]
+    chat_id = m.chat.id
+    if chat_id in QUEUE:
+        try:
+            await call_py.change_volume_call(chat_id, volume=int(range))
+            await m.reply(
+                f"✅ **تم ضبط الصوت** `{range}`%"
+            )
+        except Exception as e:
+            await m.reply(f"🚫 **ᴇʀʀᴏʀ:**\n\n`{e}`")
+    else:
+        await m.reply("❌ **لايوجد شيئ شغال**")
